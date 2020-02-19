b5e3aab3399a457ac9284eec8e2324d175e1cac1
= <=>
2020-02-19 20:14:09 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPcoredockerdemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
